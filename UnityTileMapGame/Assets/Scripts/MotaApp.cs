b3ee64eaf2c73836daf1f2a4d5f27c7899df1a2e
IGkan <544918989@qq.com>
2019-11-28 11:19:06 +0800

    28.4

--- UnityTileMapGame/Assets/Scripts/MotaApp.cs
+++ UnityTileMapGame/Assets/Scripts/MotaApp.cs
@@ -6,10 +6,12 @@ namespace Tower
 
     public class MotaApp : MonoBehaviour
     {
+        public PlayerData mModel;
         private void Awake()
         {
             ResKit.Init();
             UIMgr.SetResolution(1080, 2244, 0);
+            mModel = new PlayerData();
         }
         private void Start()
         {
